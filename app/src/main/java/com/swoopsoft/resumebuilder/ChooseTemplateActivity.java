c0669d337b5bd62f1f944b019856079198500915
jmlatham <jmlatham@yahoo.com>
2021-11-17 00:30:49 -0600

    Added a layout that includes a ScrollView and a button. Added the click listener to the button. Added the button text to the strings.xml resource file.

--- app/src/main/java/com/swoopsoft/resumebuilder/ChooseTemplateActivity.java
+++ app/src/main/java/com/swoopsoft/resumebuilder/ChooseTemplateActivity.java
@@ -3,12 +3,26 @@ package com.swoopsoft.resumebuilder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ChooseTemplateActivity extends AppCompatActivity {
+
+    Button btnBuildTemplate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_template);
+
+        btnBuildTemplate = findViewById(R.id.btnChooseTemplateBuildTemplate);
+
+        btnBuildTemplate.setOnClickListener(view -> {
+            openBuildTemplateActivity();
+        });
+    }
+
+    private void openBuildTemplateActivity(){
+        Toast.makeText(getApplicationContext(), "Open the Activity for building a Template", Toast.LENGTH_SHORT).show();
     }
 }
