77a3fb28caaf0818959d1f97eb9f913a7071471d
jmlatham <jmlatham@yahoo.com>
2021-11-16 23:13:44 -0600

    Updated the HomeActivity to have the basic layout and buttons needed for the application. The two buttons are Add Data and Add New Document. The TextViews are merely there to be place holders to see the ScrollView in action. The Linear Layout within the ScrollView will hold the created documents. These will be generated dynamically from the data in the Firebase FireStore.

--- app/src/main/java/com/swoopsoft/resumebuilder/HomeActivity.java
+++ app/src/main/java/com/swoopsoft/resumebuilder/HomeActivity.java
@@ -3,12 +3,33 @@ package com.swoopsoft.resumebuilder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button btnAddNewDocument;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnAddData = findViewById(R.id.btnHomeAddData);
+        btnAddNewDocument = findViewById(R.id.btnHomeAddNewDocument);
+
+        btnAddData.setOnClickListener(view -> addData());
+        btnAddNewDocument.setOnClickListener(view -> addDocument());
+
+    }
+
+    private void addData() {
+        Toast.makeText(getApplicationContext(), "Navigate to Add Data Activity", Toast.LENGTH_SHORT).show();
+    }
+
+    private void addDocument() {
+        Toast.makeText(getApplicationContext(), "Navigate to Add Document Activity", Toast.LENGTH_SHORT).show();
     }
 }
